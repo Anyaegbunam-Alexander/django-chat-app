d9304bce54daaae8ab0fd23846ffd289be889a75
Alexander Anyaegbunam <108991664+Anyaegbunam-Alexander@users.noreply.github.com>
2024-02-06 07:57:11 +0000

    filtering servers

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # External
+    "rest_framework",
     "drf_spectacular",
     # Internal
     "server",
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from unittest import result
+from rest_framework import status, viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from server.models import Server
+from server.serializers import ServerSerializer
+from rest_framework.exceptions import NotFound, ValidationError, NotAuthenticated
 
-# Create your views here.
+
+class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
+
+    def list(self, request: Request) -> Response:
+        category = request.query_params.get("category")
+        quantity = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+        server_id = request.query_params.get("server_id")
+
+        if by_user or server_id and not request.user.is_authenticated:
+            raise NotAuthenticated()
+
+        queryset = self.queryset
+
+        if by_user:
+            user = request.user
+            queryset = queryset.filter(members=user)
+            pass
+
+        if category:
+            queryset = queryset.filter(category__name=category)
+
+        if server_id:
+            try:
+                queryset = queryset.filter(id=int(server_id))
+                if not queryset.exists():
+                    raise NotFound(f"Server with {server_id} not found")
+
+            except Exception:
+                raise ValidationError("Invalid Server id")
+
+        if quantity:
+            queryset = queryset[: int(quantity)]
+
+        serializer = ServerSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
