bdeb728da9cec7658cd0c85034f725d901d363a1
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-02-07 23:53:07 +0100

    handle images on update or delete

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,13 +1,36 @@
 from account.models import Account
 from django.db import models
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+
+
+def category_icon_upload_path(instance, filename):
+    return f"categories/{instance.id}/icons/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, default="")
+    icon = models.FileField(upload_to=category_icon_upload_path, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+
+        return super().save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
 
 class Server(models.Model):
