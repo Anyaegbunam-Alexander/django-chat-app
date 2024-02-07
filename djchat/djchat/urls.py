217992bf5e6409e7e922953e60cb6c19bc1e833b
Alexander Anyaegbunam <108991664+Anyaegbunam-Alexander@users.noreply.github.com>
2024-02-07 22:02:08 +0000

    add media settings

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from server.views import ServerListViewSet
@@ -12,3 +14,7 @@ urlpatterns = [
     path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/ui/", SpectacularSwaggerView.as_view()),
 ] + router.urls
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
